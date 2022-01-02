3e9a2660379b645e6cb0aaa448097fc6325e618c
Louis Drotos <louis.drotos@gmail.com>
2022-01-02 18:23:23 -0500

    Updated station map to change station point style when station filter is updated

--- src/components/stations/StationsMap.vue
+++ src/components/stations/StationsMap.vue
@@ -4,7 +4,8 @@
 
 <script>
 import { mapGetters } from 'vuex';
-import L from 'mapbox.js'
+import L from 'mapbox.js';
+import find from 'lodash/find';
 
 export default {
   name: 'StationsMap',
@@ -16,38 +17,53 @@ export default {
   }),
   computed: {
     ...mapGetters({
-      stations:'stations/getStationsAll',
+      stationsAll:'stations/getStationsAll',
+      stationsActive: 'stations/getStationsSubSet',
       accessToken:'map/getMapboxAccessToken',
       baseMap:'map/getBaseMap',
       mapCenter:'map/getMapCenter',
       zoomLevel:'map/getZoomLevel',
       stationStyle: 'map/getStationStyle',
+      stationStyleGrey: 'map/getGreyOutStyle',
       stationRadius: 'map/getStationRadius'
     })
   },
   methods: {
     createMap: function() {
-      // Creates the map
       L.mapbox.accessToken = this.accessToken;
       this.map = L.mapbox.map(this.mapId)
         .setView(this.mapCenter, this.zoomLevel)
         .addLayer(L.mapbox.styleLayer(this.baseMap))
 
-      // Creates the station layer
       let stationMarkers = [];
-      this.stations.forEach(station => {
+      this.stationsAll.forEach(station => {
+        let style = this.stationStyle;
+        style['className'] = station.stationNumber;
         stationMarkers.push(
           L.circleMarker([station.latitude, station.longitude])
-            .setStyle(this.stationStyle)
+            .setStyle(style)
             .setRadius(this.stationRadius)
         )
       });
       this.stationLayer = L.layerGroup(stationMarkers);
       this.stationLayer.addTo(this.map);
+    },
+    updateStationPointStyles: function() {
+      this.stationLayer.eachLayer((station) => {
+        let stationNum = station.options.className;
+        let newStyle = find(this.stationsActive, ['stationNumber', stationNum]) ? this.stationStyle : this.stationStyleGrey;
+        newStyle['className'] = stationNum;
+        station.setStyle(newStyle);
+      })
     }
   },
   mounted: function() {
     this.createMap();
+  },
+  watch: {
+    stationsActive() {
+      this.updateStationPointStyles();
+    }
   }
 }
 </script>
